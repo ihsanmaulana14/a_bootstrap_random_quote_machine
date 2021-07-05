306210624a0e6215d5a37da65f5024dc05f5b39d
ihsan maulana <imaul7853@gmail.com>
2021-07-05 07:33:34 +0700

    Add random quote machine

---
+++ quotes.js
@@ -0,0 +1,54 @@
+const QUOTEBANK = [
+    {
+        quote:"Pekerjaan hebat tidak dilakukan dengan kekuatan, tapi dengan ketekunan.",
+        author:"Samuel Johnson"
+    },
+    {
+        quote:"Tidak mungkin ada persahabatan tanpa ada kepercayaan dan tidak ada kepercayaan tanpa integritas.",
+        author:"Samuel Johnson"
+    },
+    {
+        quote:"Penipuan dan kebohongan hanyalah ujian yang mengerikan. Kebenaran yang akan mengundangnya.",
+        author:"Samuel Johnson"
+    },
+    {
+        quote:"Tak melakukan apa-apa merupakan kekuatan setiap orang.",
+        author:"Samuel Johnson"
+    },
+    {
+        quote:"Semua kemajuan intelektual muncul saat waktu senggang.",
+        author:"Samuel Johnson"
+    },
+    {
+        quote:"Perkawinan menyimpan derita, namun hidup melajang tidak memiliki kesenangan.",
+        author:"Samuel Johnson"
+    },
+    {
+        quote:"Kepercayaan diri adalah syarat utama dalam mengemban tanggungjawab besar.",
+        author:"Samuel Johnson"
+    },
+    {
+        quote:"Temanku tersayang, bebaskan pikiranmu dari hal-hal yang tidak dapat kamu lakukan.",
+        author:"Samuel Johnson"
+    },
+    {
+        quote:"Kita cenderung mempercayai apapun yang tidak kita ketahui sama sekali, karena semua itu tidak pernah membuat kita merasa dibohongi.",
+        author:"Samuel Johnson"
+    },
+    {
+        quote:"Keberanian adalah yang terbesar dari semua kebajikan. Karena jika Anda belum berani, Anda mungkin tidak memiliki kesempatan untuk menggunakan apa pun yang lain.",
+        author:"Samuel Johnson"
+    },
+    {
+        quote: "Sukses berjalan dari satu kegagalan ke kegagalan yang lain, tapi kita kehilangan semangat",
+        author:"Abraham Lincoln"
+    },
+    {
+        quote: "Pastikan anda meletakan kaki anda di tempat yang tepat, kemudian berdirilah dengan teguh",
+        author:"Abraham Lincoln"
+    },
+    {
+        quote: "Jadilah ukuran kualitas. Beberpaa orang tak terbiasa dengan lingkungan yang menuntut hal luar biasa dari mereka.",
+        author:" Steve Jobs"
+    }
+];
